23d2a3e7528bf394ec99bbc283ec2d0238194eb5
Kamel <a.kamel.square@gmail.com>
2019-07-13 11:12:48 +0200

    Quelques changement de la class Configuration et du fichier config.properties.
    Utilisation de "RsourceBundle" pour la configuration.

--- src/main/java/fr/escape/Configuration.java
+++ src/main/java/fr/escape/Configuration.java
@@ -1,42 +1,36 @@
 package fr.escape;
 
-
+import java.util.ResourceBundle;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
+import java.util.ResourceBundle;
 
 public class Configuration {
+	
+	static ResourceBundle bundle = ResourceBundle.getBundle("config");
+	
 	public static void main(final String[] args) {
-
-		final Properties prop = new Properties();
-		InputStream input = null;
-
-		try {
-
-			input = new FileInputStream("src/main/resources/config.properties");
-
-			// load a properties file
-			prop.load(input);
-
-			// get the property value and print it out
-			System.out.println(prop.getProperty("db.url"));
-			System.out.println(prop.getProperty("db.username"));
-			System.out.println(prop.getProperty("db.password"));
-			System.out.println("Nombre par défaut est: " + prop.getProperty("nombre.defaut"));
-			
-
-		} catch (final IOException ex) {
-			ex.printStackTrace();
-		} finally {
-			if (input != null) {
-				try {
-					input.close();
-				} catch (final IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-
+		
+		System.out.println("Nombre par défaut est : " + bundle.getString("nb.chif.combi"));
+		System.out.println("Le nombre d'essai maximum est de : " + getNbEssai());
+		System.out.println("Mode développeur : " + modeDev());
+		
 	}
+	
+	public static boolean modeDev() {
+		if(bundle.getString("mode.developpeur").equals("oui")) {
+            return true;
+        }
+        else {
+            return false;
+        }
+	}
+	
+	public static int getNbEssai() {
+		String nbEssai = bundle.getString("essai");
+		return Integer.parseInt(nbEssai);
+	}
+	
 }
